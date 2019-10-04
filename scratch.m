5589ade32fd56497c11c9e1ab12649f77f00b2d7
Rory Conlin <roryconlin@gmail.com>
2019-10-04 00:00:06 -0400

    Auto-commit: Fri Oct  4 00:00:06 EDT 2019

--- scratch.m
+++ scratch.m
@@ -1,5 +1,6 @@
-% syms F G real
-% syms K
+clear
+clc
+
 syms f11 f22 g11 g22  real
 syms f12 g12 k11 k12 k21 k22
 F = [f11, f12; conj(f12), f22];
@@ -7,11 +8,19 @@ G = [g11, g12; conj(g12), g22];
 K = [k11,k12;k21,k22];
 U = sym('U',[4,4]);
 L = [-F^-1*K, F^-1; G-K'*F^-1*K, K'*F^-1];
-% L= [0 1/F; G 0];
 J = [zeros(2),eye(2);-eye(2),zeros(2)];
 simplify(J*L*J-L')
 pretty(simplify(trace(U^-1*L*U)))
 
+
+%%
+syms F G real
+syms K
+U = sym('U',[2,2]);
+L= [0 1/F; G 0];
+J = [zeros(1),eye(1);-eye(1),zeros(1)];
+simplify(J*L*J-L')
+pretty(simplify(trace(U^-1*L*U)))
 
 %%
 syms a b c d f2 z sqrtD
